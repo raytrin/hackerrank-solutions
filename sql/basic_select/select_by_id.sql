a33deee4239405d24d3666f31462740f9d49fab8
raytrin <159145067+raytrin@users.noreply.github.com>
2025-08-03 21:59:22 -0300

    Adiciona o desafio Select by ID de SQL

---
+++ sql/basic_select/select_by_id.sql
@@ -0,0 +1,7 @@
+-- Problem: Select by ID
+-- Link: https://www.hackerrank.com/challenges/select-by-id/problem
+-- Descrição: Selecionar todas as colunas de uma cidade, na tabela CITY, cujo ID seja 1661.
+
+SELECT *
+FROM city
+WHERE ID = 1661;
