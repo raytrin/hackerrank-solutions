fbe8c5e07793a78aa809987028b8b3a70a7dcfd8
raytrin <159145067+raytrin@users.noreply.github.com>
2025-11-14 01:55:04 -0300

    Adiciona o desafio Weather Observation Station 10

--- sql/basic_select/weather_observation_station_10.sql
+++ sql/basic_select/weather_observation_station_10.sql
@@ -1,5 +1,6 @@
 -- Problem: Weather Observation Station 10
--- Link: 
+-- Link: https://www.hackerrank.com/challenges/weather-observation-station-11/problem
+-- Descrição: Seleciona a o nome da cidade que termine com vogal
 
 SELECT DISTINCT city
 FROM STATION
